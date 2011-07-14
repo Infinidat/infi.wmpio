30b145ccd2b21648d38b63cbc58a23057e01f891
Guy Rozendorn <guy@rzn.co.il>
2011-07-14 10:50:23 +0300

    more work

--- src/infi/wmpio/scripts/__init__.py
+++ src/infi/wmpio/scripts/__init__.py
@@ -1,3 +1,23 @@
 
+MPIO_WMI_NAMESPACE = r'root\wmi'
+DEVICES_QUERY = 'SELECT * FROM MPIO_GET_DESCRIPTOR'
+
+from bunch import Bunch
+
+class MpioGetDescriptorEntry(Bunch):
+    def __init__(self):
+        super(MpioGetDescriptorEntry, self).__init__()
+        self.DeviceName = None
+        self.InstanceName = None
+        self.NumberPdos = None
 def travel():
-    pass
+    from wmi import WMI
+    client = WMI(namespace=MPIO_WMI_NAMESPACE, find_classes=False)
+    query = client.query(DEVICES_QUERY)
+    results = []
+    for device in query:
+        item = MpioGetDescriptorEntry()
+        for attr in ['DeviceName', 'InstanceName', 'NumberPdos']:
+            setattr(item, attr, getattr(device, attr))
+        results.append(item)
+    print results
